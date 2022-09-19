7ebfffe9e070ddbed487ead68d91160463c0fc4f
donovan6000 <nicolasflamel@mwcwallet.com>
2022-09-19 00:05:39 -0700

    Fixes calling functions that require crypto

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Use strict
 "use strict";
 
+// Check if using React Native
+if(typeof navigator !== "undefined" && navigator["product"] === "ReactNative") {
+
+	// Throw error
+	throw "Crypto not supported on platform";
+}
+
+// Set global crypto
+global["crypto"] = require("crypto");
+
 // Exports
 module["exports"] = require("bindings")("secp256k1_zkp.node");
